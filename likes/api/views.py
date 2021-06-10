1357b8349d514aa74e22e56493a5f702bc438cbe
hy2610 <hy2610@columbia.edu>
2021-06-10 12:56:19 -0400

    likes cancel api

--- likes/api/views.py
+++ likes/api/views.py
@@ -1,12 +1,14 @@
 from likes.api.serializers import (
     LikeSerializer,
     LikeSerializerForCreate,
+    LikeSerializerForCancel,
 )
 from likes.models import Like
 from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from utils.decorators import require_params
+from rest_framework.decorators import action
 
 
 class LikeViewSets(viewsets.GenericViewSet):
@@ -30,3 +32,18 @@ class LikeViewSets(viewsets.GenericViewSet):
             LikeSerializer(instance).data,
             status=status.HTTP_201_CREATED,
         )
+
+    @action(methods=['POST'], detail=False)
+    @require_params(requirement_attr='data', params=['content_type', 'object_id'])
+    def cancel(self, request, *args, **kwargs):
+        serializer = LikeSerializerForCancel(
+            data=request.data,
+            context={'request': request},
+        )
+        if not serializer.is_valid():
+            return Response({
+                'message': 'Please check input',
+                'errors': serializer.errors,
+            }, status=status.HTTP_400_BAD_REQUEST)
+        serializer.cancel()
+        return Response({'success': True}, status=status.HTTP_200_OK)
